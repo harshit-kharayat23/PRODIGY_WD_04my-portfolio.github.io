d69851a3478151ae404aa5fd75ed6c22897430f7
harshit-kharayat23 <harshitkharayat224@gmail.com>
2024-02-14 20:51:27 +0530

    second commit

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
-let sections=document.querySelectorAll("section");
-let navlinks=document.querySelectorAll("header nav  a");
-window.onscroll=()=>{
-    sections.forEach(sec=>{
-        let top=window.scrollY;
-        let offset=sec.offsetTop-150;
-        let height=sec.offsetHeight;
-        let id=sec.getAttribute('id');
-        if(top>=offset && top <offset+height)
-        {
-                navlinks.forEach(links=>{
-                    links.classList.remove("active");
-                    document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
-                });
-        }
-    });
+// menu icon
+let menuIcon=document.querySelector("#menu-icon");
+let navbar=document.querySelector(".navbar");
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
 };
+
+
+// smoothScroll
+function smoothScroll(target) {
+    const element = document.querySelector(target);
+    if (element) {
+        window.scrollTo({
+            top: element.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
+
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        smoothScroll(link.getAttribute('href'));
+    });
+});
+
+// sticky nav bar
 window.onscroll=()=>{
     let header=document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY > 100);
-
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
-let mode=document.querySelector(".mode");
-let body=document.querySelector("body");
-let at=document.querySelector(" nav a");
-let flag=0;
-mode.addEventListener("click",()=>{
-    if(flag==0)
-    {
-        mode.innerHTML="<i class='bx bxs-moon' ></i>";
-        flag=1;
-        body.style.background="White";
-        body.style.color="black";
-        header.sticky.style.background="white";
-        
-    }
-    else if(flag=1)
-    {
-        mode.innerHTML="<i class='bx bx-sun'></i>";
-        flag=0;
-        body.style.background="var(--bg-color)";
-        body.style.color="white";
-        at.style.color="--var(--main-color)";
-    }
+// scroll reveal
+ScrollReveal({ 
+    // reset: true,
+    distance:"80px",
+    duration:2000,
+    delay:100
+
 });
+ScrollReveal().reveal('.home-content, .heading', { origin:"top" });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:"bottom" });
+ScrollReveal().reveal('.home-content h1, .about img', { origin:"left" });
+ScrollReveal().reveal('.home-content p, .about-content', { origin:"right"});
+
+
+
